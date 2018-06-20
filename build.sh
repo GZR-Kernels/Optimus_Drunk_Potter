da073eaa122533321cf66324fa714c948a7c2c72
GtrCraft <nickvanbokhorst@hotmail.com>
2018-06-20 16:21:26 +0200

    Fix build script
    * Also make it work with clang

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ DATE_POSTFIX=$(date +"%Y%m%d")
 
 ## Copy this script inside the kernel directory
 KERNEL_DIR=$PWD
-KERNEL_TOOLCHAIN=/home/gtrcraft/data/validus/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+KERNEL_TOOLCHAIN=/home/nick/data/validus/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CLANG_TOOLCHAIN=/home/nick/data/validus/prebuilts/clang/host/linux-x86/7.0-DragonTC/bin/clang
 KERNEL_DEFCONFIG=potter_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
-JOBS=16
+JOBS=28
 ANY_KERNEL2_DIR=$KERNEL_DIR/AnyKernel2/
 FINAL_KERNEL_ZIP=Optimus_Drunk_Potter-$DATE_POSTFIX.zip
 # Speed up build process
@@ -26,26 +27,27 @@ echo "**** Setting Toolchain ****"
 export CROSS_COMPILE=$KERNEL_TOOLCHAIN
 export ARCH=arm64
 export SUBARCH=arm64
+export KBUILD_COMPILER_STRING="DragonTC clang version 7.0"
 
 # Clean build always lol
 echo "**** Cleaning ****"
-make clean && make mrproper
+make clean && make mrproper && rm -rf out/
 
 echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
 echo -e "$blue***********************************************"
 echo "          BUILDING KERNEL          "
 echo -e "***********************************************$nocol"
-make $KERNEL_DEFCONFIG
-make -j$JOBS
+make $KERNEL_DEFCONFIG O=out
+make -j$JOBS CC=$CLANG_TOOLCHAIN CLANG_TRIPLE=aarch64-linux-android- O=out
 
 echo -e "$blue***********************************************"
 echo "          GENERATING DT.img          "
 echo -e "***********************************************$nocol"
-$DTBTOOL/dtbToolCM -2 -o $KERNEL_DIR/arch/arm64/boot/dtb -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm64/boot/dts/qcom/
+$DTBTOOL/dtbToolCM -2 -o $KERNEL_DIR/out/arch/arm64/boot/dtb -s 2048 -p $KERNEL_DIR/out/scripts/dtc/ $KERNEL_DIR/out/arch/arm64/boot/dts/qcom/
 
 echo "**** Verify Image.gz & dtb ****"
-ls $KERNEL_DIR/arch/arm64/boot/Image.gz
-ls $KERNEL_DIR/arch/arm64/boot/dtb
+ls $KERNEL_DIR/out/arch/arm64/boot/Image.gz
+ls $KERNEL_DIR/out/arch/arm64/boot/dtb
 
 #Anykernel 2 time!!
 echo "**** Verifying Anyernel2 Directory ****"
@@ -56,14 +58,14 @@ rm -rf $ANY_KERNEL2_DIR/Image.gz
 rm -rf $ANY_KERNEL2_DIR/$FINAL_KERNEL_ZIP
 
 echo "**** Copying Image.gz ****"
-cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANY_KERNEL2_DIR/
+cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz $ANY_KERNEL2_DIR/
 echo "**** Copying dtb ****"
-cp $KERNEL_DIR/arch/arm64/boot/dtb $ANY_KERNEL2_DIR/
+cp $KERNEL_DIR/out/arch/arm64/boot/dtb $ANY_KERNEL2_DIR/
 
 echo "**** Time to zip up! ****"
 cd $ANY_KERNEL2_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-cp $KERNEL_DIR/AnyKernel2/$FINAL_KERNEL_ZIP /home/gtrcraft/data/kernel/$FINAL_KERNEL_ZIP
+cp $KERNEL_DIR/AnyKernel2/$FINAL_KERNEL_ZIP /home/nick/data/kernel/$FINAL_KERNEL_ZIP
 
 echo "**** Good Bye!! ****"
 cd $KERNEL_DIR
