cdafe100ad5c3251ef5644a7b809a306c34a1086
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-01-09 12:43:37 +0000

    wakelock: block some wakelocks

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,9 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			""
+#define LIST_WL_DEFAULT			"qcom_rx_wakelock;wlan;netmgr_wl;NETLINK;IPA_WS;pil-a506_zap;50028"
 
 #define LENGTH_LIST_WL			255
-#define LENGTH_LIST_WL_DEFAULT		100
+#define LENGTH_LIST_WL_DEFAULT		70
+
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
