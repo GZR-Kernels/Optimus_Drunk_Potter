cfdbe7deb4da976389d69a7a5f86b71f586faa77
utsavisgreat <utsavbalar1231@gmail.com>
2019-04-03 09:33:41 +0000

    kernel: boost to max for zygote forks only for 100ms

--- kernel/fork.c
+++ kernel/fork.c
@@ -1707,10 +1707,10 @@ long do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 1250 ms when userspace launches an app */
+	/* Boost CPU to the max for 100 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(1250);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1250);
+		cpu_input_boost_kick_max(100);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 	}
 
 	/*
