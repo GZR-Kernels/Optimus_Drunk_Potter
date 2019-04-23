0fb212194706a425d5b1fe493b121481327dea93
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-04-23 09:32:36 +0000

    fpc1020: Fix incorrect FP_HAL_BIN defination
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- include/linux/fpc1020.h
+++ include/linux/fpc1020.h
@@ -1,7 +1,7 @@
 #ifndef _FPC1020_H
 #define _FPC1020_H
 
-#define FP_HAL_BIN "/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service"
+#define FP_HAL_BIN "/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-fpcservice"
 extern atomic_t fp_hal_pid;
 
 #endif
