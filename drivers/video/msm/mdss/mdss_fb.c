c474e3c2a64483afe594699a517e382bb23ae743
Nick van Bokhorst <nickvanbokhorst@hotmail.com>
2019-05-03 09:07:54 +0200

    potter: Remove old klapse
    Revert "klapse:Adapt for potter"
    
    This reverts commit cff55ccf34818113e62c0c4128b0ae04c25607f1.
    
    Revert "KLAPSE: Modernize sysfs to prevent CFI failure"
    
    This reverts commit c3e678491880a9b9ccf7c80b7a424537471481c0.
    
    Revert "KLAPSE: adapt to work with kcal for mdss_mdp_v1_7"
    
    This reverts commit 11203b8530b7d5e27f970651434d4170e646333b.
    
    Revert "Introducing KLapse - A kernel level livedisplay module v4.3"
    
    This reverts commit 80efc2c461d05c092d971a5cadddf09605021011.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -58,10 +58,6 @@
 #include "mdss_mdp.h"
 #include "mdp3_ctrl.h"
 
-#ifdef CONFIG_KLAPSE
-#include "klapse.h"
-#endif
-
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -339,9 +335,6 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 		mdss_fb_set_backlight(mfd, bl_lvl);
 		mutex_unlock(&mfd->bl_lock);
 	}
-#ifdef CONFIG_KLAPSE
-	set_rgb_slider(bl_lvl);
-#endif
 }
 
 static struct led_classdev backlight_led = {
